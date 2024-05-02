91d0b1db7eb2923436a6df8afb8c978591c7185c
Aleksey Abramov <lomberjos33hg@gmail.com>
2024-05-02 12:42:08 +0300

    created company-rating component

--- src/components/Main/Aside/CompanyRating/index.tsx
+++ src/components/Main/Aside/CompanyRating/index.tsx
@@ -1,11 +1,51 @@
 import Accordion from "@/components/Ui/Accordion";
+import { useState } from "react";
+
+type CheckboxProps = {
+    id: number;
+    checked: boolean;
+    onChange: (event: React.ChangeEvent<HTMLInputElement>) => void;
+};
+
+const CheckboxStar = ({id, checked, onChange}: CheckboxProps): JSX.Element => {
+    return (
+        <div className="checkbox-block">
+            <input type="checkbox" id={`star-${id}`} checked={checked} onChange={onChange} />
+            <label htmlFor={`star-${id}`} className="flex">
+                {[...Array(5)].map((_, index) => (
+                    <span key={index}>
+                        <img src={`/img/svg/${id < 6 - index ? 'fill-star' : 'star'}.svg`} alt="" />
+                    </span>
+                ))}
+            </label>
+        </div>
+    )
+}
 
 const CompanyRating = () => {
+    const [starData, setStarData] = useState<Record<string, boolean>>({});
+
+    const handleCheckboxChange = (name: string) => {
+        setStarData(prevState => ({
+            ...prevState,
+            [name]: !prevState[name]
+        }));
+    }
+
     return (
         <Accordion
         title={'Company rating'}
         >
-            home
+            <div className="flex flex-col gap-3">
+                {[1, 2, 3, 4, 5].map(num => (
+                    <CheckboxStar
+                    key={num} 
+                    id={num}
+                    checked={starData[`star${num}`] || false}
+                    onChange={() => handleCheckboxChange(`star${num}`)}
+                    />
+                ))}
+            </div>
         </Accordion>
     )
 }
